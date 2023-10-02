5183de753d561e95713271721821ee42c9b0b159
hoangdat12 <01658384066a>
2023-10-02 17:07:38 +0700

    second

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
-import app from './src/app'
+import app from "./src/app";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log("Server start with http://localhost:8080");
+  console.log(`Server start with http://localhost:${PORT}`);
 });
